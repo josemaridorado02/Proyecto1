57b5d7dc0cf965438d08fc8b253e7f83e2969a18
josemaridorado02 <josemariaoteodorado12@gmail.com>
2025-11-29 18:14:53 +0100

    Cuarto cambio crear otro servidor con docker y usar servidorweb como proxy inverso

--- opentofu/escenario.tf
+++ opentofu/escenario.tf
@@ -72,5 +72,21 @@ locals {
       user_data      = "${path.module}/cloud-init/server2/user-data.yaml"
       network_config = "${path.module}/cloud-init/server2/network-config.yaml"
     }
+
+    docker = {
+      name       = "docker"
+      memory     = 1024
+      vcpu       = 1
+      base_image = "debian13-base.qcow2"
+
+      networks = [
+        { network_name = "red-externa", wait_for_lease = true },
+        { network_name = "red-conf" },
+        { network_name = "red-datos" }
+      ]
+
+      user_data      = "${path.module}/cloud-init/server3/user-data.yaml"
+      network_config = "${path.module}/cloud-init/server3/network-config.yaml"
+    }
   }
 }
